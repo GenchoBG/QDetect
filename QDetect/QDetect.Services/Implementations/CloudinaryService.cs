7741b5544c3c84c1234d00a51127e4ae4b87ea81
KostadinovK <kostadinovk@gmail.com>
2020-03-27 21:46:55 +0200

    Added CloudinaryService for uploading pictures

---
+++ QDetect/QDetect.Services/Implementations/CloudinaryService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using QDetect.Services.Interfaces;
+
+namespace QDetect.Services.Implementations
+{
+    public class CloudinaryService : ICloudinaryService
+    {
+        private readonly Cloudinary cloudinaryUtility;
+
+        public CloudinaryService(Cloudinary cloudinaryUtility)
+        {
+            this.cloudinaryUtility = cloudinaryUtility;
+        }
+
+        public async Task<string> UploadPictureAsync(IFormFile pictureFile, string fileName)
+        {
+            byte[] destinationData;
+
+            using (var ms = new MemoryStream())
+            {
+                await pictureFile.CopyToAsync(ms);
+                destinationData = ms.ToArray();
+            }
+
+            UploadResult uploadResult = null;
+
+            using (var ms = new MemoryStream(destinationData))
+            {
+                ImageUploadParams uploadParams = new ImageUploadParams
+                {
+                    Folder = "ad_images",
+                    File = new FileDescription(fileName, ms)
+                };
+
+                uploadResult = this.cloudinaryUtility.Upload(uploadParams);
+            }
+
+            return uploadResult?.SecureUri.AbsoluteUri;
+        }
+    }
+}
---
+++ QDetect/QDetect.Services/Interfaces/ICloudinaryService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace QDetect.Services.Interfaces
+{
+    public interface ICloudinaryService
+    {
+        Task<string> UploadPictureAsync(IFormFile pictureFile, string fileName);
+    }
+}
