689f4729052b92fe932694b71b71921968ac1fb0
Owliie <sladurcheto5232@abv.bg>
2020-03-28 17:57:11 +0200

    Added modal for visualization of details of people

---
+++ QDetect/QDetect.Web/wwwroot/js/search.js
@@ -0,0 +1,35 @@
+﻿function openModal(person) {
+    console.log(person);
+    $("#modalCenterTitle").text(person.name);
+    person.hasReports ? $("#modalReported").text("!") : $("#modalReported").css("display", "none");
+    $("#modalImage").attr("src", person.image);
+    $("#tableName").text(person.name);
+    $("#tableCity").text(person.city);
+    $("#tableQuarantine").text(person.quarantineEndDate);
+    $("#tableReported").text(person.hasReports ? "Yes" : "No");
+}
+
+$(document).ready(() => {
+    $('body').on('click', '[data-editable]', function () {
+
+        var el = $(this);
+
+        var input = $('<input/>').val(el.text());
+        el.replaceWith(input);
+
+        var save = function () {
+            var td = $(`<td data-editable id=${$(this).id}></td>`).text(input.val());
+            input.replaceWith(td);
+        };
+
+        /**
+          We're defining the callback with `one`, because we know that
+          the element will be gone just after that, and we don't want
+          any callbacks leftovers take memory.
+          Next time `p` turns into `input` this single callback
+          will be applied again.
+        */
+        input.one('blur', save).focus();
+
+    });
+});
