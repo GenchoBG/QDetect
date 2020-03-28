1be7d78342a1dee81d27cebf03e68df60261f4cd
GenchoBG <genchobg11@gmail.com>
2020-03-28 16:50:31 +0200

    Hosted application in Azure

---
+++ QDetect/QDetect.Web/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using QDetect.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace QDetect.Web.Infrastructure.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder Seed(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<QDetectDbContext>().Database.Migrate();
+
+                // Seed data here.
+            }
+
+            return app;
+        }
+    }
+}
--- QDetect/QDetect.Web/Startup.cs
+++ QDetect/QDetect.Web/Startup.cs
@@ -9,6 +9,7 @@ using QDetect.Data;
 using Microsoft.EntityFrameworkCore;
 using QDetect.Services.Implementations;
 using QDetect.Services.Interfaces;
+using QDetect.Web.Infrastructure.Extensions;
 
 namespace QDetect.Web
 {
@@ -61,16 +62,18 @@ namespace QDetect.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+
+            app.Seed();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
