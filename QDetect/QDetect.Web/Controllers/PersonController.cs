3c448e94651e1a6004bbcd300895f8885c1d97de
KostadinovK <kostadinovk@gmail.com>
2020-03-28 11:02:25 +0200

    Added all peoples listing method in person controller

--- QDetect/QDetect.Web/Controllers/PersonController.cs
+++ QDetect/QDetect.Web/Controllers/PersonController.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using QDetect.Services.Interfaces;
 using QDetect.Web.BindingModels;
+using QDetect.Web.ViewModels;
 
 namespace QDetect.Web.Controllers
 {
@@ -19,6 +22,25 @@ namespace QDetect.Web.Controllers
         public async Task<IActionResult> Upload()
         {
             return this.View();
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var peoples = await peopleService.GetAll().Select(p => new PeopleViewModel
+            {
+                Name = p.Name,
+                City = p.City,
+                Image = p.Images.FirstOrDefault().Image.Link,
+                QuanratineEndDate = p.QuarantineEndDate.ToLocalTime().ToString(),
+                UCN = p.UCN,
+                HasReports = p.Reports.Any()
+            }).ToListAsync();
+
+            var viewModel = new PeopleListingPageViewModel();
+
+            viewModel.Peoples = peoples;
+
+            return this.View(viewModel);
         }
     }
 }
---
+++ QDetect/QDetect.Web/Controllers/ReportController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QDetect.Services.Interfaces;
+
+namespace QDetect.Web.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IReportService reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            this.reportService = reportService;
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public Task<IActionResult> Create(int personId, int imageId)
+        {
+            return this.View();
+        }
+
+    }
+}
