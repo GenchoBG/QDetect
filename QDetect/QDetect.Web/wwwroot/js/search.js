9a8889c3438c1eae8239879ea251bc480cfa7c1d
Owliie <sladurcheto5232@abv.bg>
2020-03-28 21:21:35 +0200

    Added pagination for search index view

--- QDetect/QDetect.Web/wwwroot/js/search.js
+++ QDetect/QDetect.Web/wwwroot/js/search.js
@@ -1,4 +1,38 @@
-﻿$(document).ready(() => {
+﻿var page = 0;
+var maxPages = 0;
+var peoplePerPage = 8;
+var peopleCount = 0;
+
+$(document).ready(() => {
+    $("#search-btn").attr("disabled", true);
+    $("#prev-arrow").prop("disabled", true);
+
+    peopleCount = $("#results").children().length;
+
+    maxPages = Math.ceil(peopleCount / peoplePerPage);
+
+    if (peopleCount <= peoplePerPage) {
+        $("#search-navigation").hide();
+
+        for (var person of $("#results").children()) {
+            $(person).attr("hidden", false);
+        }
+    } else {
+        for (var i = 0; i < peoplePerPage; i++) {
+            $($("#results").children()[i]).attr("hidden", false);
+        }
+    }
+
+    $("#searchInput").on("input", () => {
+        let input = $("#searchInput").val();
+
+        if (input === "") {
+            $("#search-btn").attr("disabled", true);
+        } else {
+            $("#search-btn").attr("disabled", false);
+        }
+    });
+
     $('body').on('click', '[data-editable]', function () {
 
         var el = $(this);
@@ -93,4 +127,36 @@ function saveInfo() {
             console.log(req.responseText);
         }
     });
+}
+
+function navigatePrevious() {
+    $("#next-arrow").prop("disabled", false);
+
+    shouldShowPeople(true);
+
+    page--;
+    if (page === 0) {
+        $("#prev-arrow").prop("disabled", true);
+    }
+
+    shouldShowPeople(false);
+}
+
+function navigateNext() {
+    $("#prev-arrow").prop("disabled", false);
+
+    shouldShowPeople(true);
+
+    page++;
+    if (page === maxPages - 1) {
+        $("#next-arrow").prop("disabled", true);
+    }
+
+    shouldShowPeople(false);
+}
+
+function shouldShowPeople(show) {
+    for (let i = peoplePerPage * page; i < peoplePerPage * page + peoplePerPage; i++) {
+        $($("#results").children()[i]).attr("hidden", show);
+    }
 }
